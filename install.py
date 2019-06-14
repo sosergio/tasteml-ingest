bf2a6da9447612aec0212ce271a03cc5e4071ced
Sergio Sorrentino <sosergio@gmail.com>
2019-06-14 07:35:15 +0100

    can read from json file and upload to mongo collection

---
+++ install.py
@@ -0,0 +1,6 @@
+import nltk
+
+nltk.download() # opens the GUI to download
+
+#   downloads the NLTK's small collection of web text includes content from a Firefox discussion forum, conversations overheard in New York, the movie script of Pirates of the Carribean, personal advertisements, and wine reviews.
+#   nltk.download('webtext') 
--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 import pymongo
 import pprint
+import nltk
+import json
+
+def insertOne(doc: any, collection: pymongo.collection.Collection):
+    pprint.pprint(doc)
+    collection.insert_one(doc)
+
 
 from pymongo import MongoClient
 password = "ifhK8pwM6Uhbs8C"
@@ -7,5 +14,17 @@ username = "admin"
 client = MongoClient(f"mongodb+srv://{username}:{password}@tasteml-db-gndd2.mongodb.net/test?retryWrites=true&w=majority")
 db = client.get_database('tasteml-db')
 collection = db.get_collection('tasting-notes')
-item = collection.find_one()
-pprint.pprint(item)
+items = collection.find({})
+for doc in items:
+    pprint.pprint(doc)
+
+if False:
+    count = 0
+    with open('dataset/winemag-data-130k-v2.json') as json_file:  
+        data = json.load(json_file)
+        for note in data:
+            if(count  > 10):
+                break
+            else:
+                count= count +1
+                insertOne(note, collection)
